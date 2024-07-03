eef44e12bbed1418e2eaf7dda34d1e9237991220
dansun2 <dansunmusik7@gmail.com>
2024-07-03 12:14:37 +0900

    240703

--- chap03-association-mapping/src/test/java/com/ohgiraffers/section03/bidirection/Category.java
+++ chap03-association-mapping/src/test/java/com/ohgiraffers/section03/bidirection/Category.java
@@ -1,4 +1,27 @@
 package com.ohgiraffers.section03.bidirection;
 
+import com.ohgiraffers.section02.onetomany.Menu;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity(name = "bidirection_category")
+@Table(name = "tbl_category")
 public class Category {
+
+    @Id
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "ref_category_code")
+    private Integer refCategoryCode;
+
+    @OneToMany(mappedBy = "category")
+    private List<Menu> menuList;
+
 }
